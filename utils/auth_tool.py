06ac30ca246ee3aae5612d889d3f4cd06d8ad3b0
lizy <lizy@yex360.com>
2018-11-28 21:50:46 +0800

    fixed

--- utils/auth_tool.py
+++
@@ -1,6 +0,0 @@
-
-# from django.conf import settings
-from repository import models
-def my_auth(**kwargs):
-    user = models.User.objects.filter(**kwargs).values('blog__site','username', 'avatar', 'id').first()
-    return user
